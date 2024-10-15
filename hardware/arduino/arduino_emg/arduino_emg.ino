6a4773f25e96397eeb26cf102c9c105ecb808768
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-10-15 16:09:21 +0700

    feat(hardware): calculate RMS

--- hardware/arduino/arduino_emg/arduino_emg.ino
+++ hardware/arduino/arduino_emg/arduino_emg.ino
@@ -1,12 +1,43 @@
+int emgReadings[10];
+int currentIndex = 0;
+
 void setup() {
   Serial.begin(9600);
+  for (int i; i < 10; i++) {
+    emgReadings[i] = 0;
+  }
 }
 
 void loop() {
-  Serial.print(200);  //Tofreezethelowerlimit 
+  Serial.print(825);  //Tofreezethelowerlimit 
   Serial.print(" ");
-  Serial.print(700);  //Tofreezetheupperlimit 
+  Serial.print(840);  //Tofreezetheupperlimit 
   Serial.print(" ");
+
   int sensorValue = analogRead(A0);
-  Serial.println(sensorValue);
+  emgReadings[currentIndex] = sensorValue;
+
+  // Serial.println(sensorValue);
+  // for(int i = 0; i < 10; i++) {
+  //   // Serial.print(emgReadings[i]);
+  //   Serial.print(" ");
+  // }
+  // Serial.println("");
+
+  unsigned long squaredSum = 0;
+  for(int i = 0; i < 10; i++) {
+    squaredSum += pow(emgReadings[i], 2);
+  }
+  // Serial.println(squaredSum);
+
+  float average = squaredSum / 10;
+  float rms = sqrt(average);
+
+  Serial.print(rms);
+  Serial.println("");
+
+  currentIndex++;
+  if(currentIndex >= 10) {
+    currentIndex = 0;
+  }
 }
