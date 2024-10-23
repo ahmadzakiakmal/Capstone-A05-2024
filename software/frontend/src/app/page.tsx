dab2d3a0838796efd9d96feba1c58960bcb20e35
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-10-23 15:35:34 +0700

    feat: add steps and loading display

--- software/frontend/src/app/page.tsx
+++ software/frontend/src/app/page.tsx
@@ -1,7 +1,11 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import Logo from "@/../public/Logo.png";
+import ConnectYourDevice from "@/../public/ConnectYourDevice.png";
+import EnterIPAddress from "@/../public/EnterIPAddress.png";
+import SearchingDevice from "@/../public/SearchingDevice.png";
+
 import Button from "@/components/Button";
 import { useState } from "react";
 
@@ -9,7 +13,7 @@ export default function Home() {
   const [step, setStep] = useState<number>(0);
   return (
     <main className="bg-gradient-to-tr from-cyan-primary to-green-primary min-h-screen flex flex-col justify-center items-center">
-      <div className="bg-white/90 rounded-[10px] py-20 px-40 flex flex-col justify-center items-center gap-5">
+      <div className="bg-white/90 rounded-[10px] py-10 flex flex-col justify-center items-center gap-5 min-w-[500px]">
         {step === 0 && (
           <Landing
             onButtonClick={() => {
@@ -17,6 +21,9 @@ export default function Home() {
             }}
           />
         )}
+        {step === 1 && <Step1 onButtonClick={() => setStep((prev) => prev + 1)} />}
+        {step === 2 && <Step2 onButtonClick={() => setStep((prev) => prev + 1)} />}
+        {step === 3 && <Loading /> }
       </div>
     </main>
   );
@@ -30,6 +37,63 @@ function Landing({ onButtonClick }: { onButtonClick: () => void }) {
         alt="Logo"
       />
       <Button onClick={onButtonClick}>Start</Button>
+    </>
+  );
+}
+
+function Step1({ onButtonClick }: { onButtonClick: () => void }) {
+  return (
+    <>
+      <Image
+        src={ConnectYourDevice}
+        alt="Connect Your Device"
+      />
+      <div className="flex flex-col gap-[10px]">
+        <h1 className="text-[24px] text-dark-1 font-semibold text-center">Connect Your Device</h1>
+        <p className="max-w-[400px] text-dark-1 text-center">
+          Connect this device and the EMG device to the same Wi-Fi network. Once done, click Continue.
+        </p>
+      </div>
+      <Button onClick={onButtonClick}>Continue</Button>
+    </>
+  );
+}
+
+function Step2({ onButtonClick }: { onButtonClick: () => void }) {
+  return (
+    <>
+      <Image
+        src={EnterIPAddress}
+        alt="Connect Your Device"
+      />
+      <div className="flex flex-col gap-[10px]">
+        <h1 className="text-[24px] text-dark-1 font-semibold text-center">Enter the IP Address</h1>
+        <p className="max-w-[400px] text-dark-1 text-center">
+          Enter the IP Address of the EMG device shown on its screen.
+        </p>
+        <input
+          type="text"
+          className="bg-[#d4e4de] px-2 !outline-none !text-black w-full text-center py-[10px] text-[24px] font-medium"
+        />
+      </div>
+      <Button onClick={onButtonClick}>Continue</Button>
+    </>
+  );
+}
+
+function Loading() {
+  return (
+    <>
+      <Image
+        src={SearchingDevice}
+        alt="Connect Your Device"
+      />
+      <div className="flex flex-col gap-[10px]">
+        <h1 className="text-[24px] text-dark-1 font-semibold text-center">Looking for Device...</h1>
+        <p className="max-w-[400px] text-dark-1 text-center">
+          Please wait while we attempt to connect to the EMG device...
+        </p>
+      </div>
     </>
   );
 }
