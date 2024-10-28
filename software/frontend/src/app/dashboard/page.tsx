537b2025a46a9d27f31bed469a9f80bd34b12159
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-10-28 13:26:18 +0700

    feat: create Navbar component

--- software/frontend/src/app/dashboard/page.tsx
+++ software/frontend/src/app/dashboard/page.tsx
@@ -1,43 +1,10 @@
 "use client";
-import Button from "@/components/Button";
-import { useEffect } from "react";
+import Navbar from "@/components/Navbar";
 
 export default function Dashboard() {
-  async function getPorts() {
-    if ("serial" in navigator) {
-      const ports = await navigator.serial.getPorts();
-      console.log(ports);
-    }
-  }
-  useEffect(() => {
-    getPorts();
-  });
   return (
-    <>
-      Dashboard
-      <Button
-        onClick={async () => {
-          const port = await navigator.serial.requestPort();
-          console.log(port);
-          await port.open({ baudRate: 9600 });
-          const reader = port.readable.getReader();
-
-          // Listen to data coming from the serial device.
-          for (let i = 0; i < 10; i ++) {
-            setTimeout(() => {}, 500)
-            const { value, done } = await reader.read();
-            if (done) {
-              // Allow the serial port to be closed later.
-              reader.releaseLock();
-              break;
-            }
-            // value is a Uint8Array.
-            console.log(value);
-          }
-        }}
-      >
-        Test
-      </Button>
-    </>
+    <main className="bg-[#F3F4F6] min-h-screen">
+      <Navbar />
+    </main>
   );
 }
--- software/frontend/src/components/Navbar.tsx
+++ software/frontend/src/components/Navbar.tsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+import Logo from "@/../public/Logo.png"
+
 export default function Navbar() {
   return(
-    <></>
+    <nav className="flex flex-row justify-between items-center bg-gradient-to-tr from-green-primary to-cyan-primary px-6 py-3">
+      <Image src={Logo} alt="Logo" className="size-[80px]" />
+
+      <button className="flex flex-col gap-[5px]">
+        <div className="h-[7px] w-10 bg-dark-1 rounded-full" />
+        <div className="h-[7px] w-10 bg-dark-1 rounded-full" />
+        <div className="h-[7px] w-10 bg-dark-1 rounded-full" />
+      </button>
+    </nav>
   )
 }
