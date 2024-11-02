2d411823f703d994832eff8f20306275d30029ea
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-11-02 10:31:44 +0700

    feat: restructure backend folder

--- software/backend/express/src/index.js
+++ software/backend/express/src/index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 5000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile("views/index.html", {root: __dirname})
 })
 
 app.listen(port, () => {
-  console.log(`Server runing on port ${port}`)
+  console.log(`MyoSense server runing on port ${port}`)
 })
