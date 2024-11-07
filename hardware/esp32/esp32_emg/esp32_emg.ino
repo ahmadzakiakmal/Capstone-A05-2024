cbbaf0919b18c9ec03040b41a7106ef3c030ffc2
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-11-07 12:12:25 +0700

    feat: update hardware display

--- hardware/esp32/esp32_emg/esp32_emg.ino
+++ hardware/esp32/esp32_emg/esp32_emg.ino
@@ -2,10 +2,6 @@
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <PubSubClient.h>
-// #include <ESPAsyncWebServer.h>
-// #include "LittleFS.h"
-// #include <Arduino_JSON.h>
-// #include <Adafruit_Sensor.h>
 
 // Display Libraries
 #include <lvgl.h>
@@ -27,37 +23,45 @@ const char* mqtt_password = "myosense314159";
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+lv_obj_t *wifi_status_label;
+lv_obj_t *mqtt_status_label; 
+
 void setup() {
   Serial.begin(115200);
-  // EMG Setup
   pinMode(EMG_PIN, INPUT);
-
-  // WiFi & MQTT Setup
-  Serial.println("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-  delay(1000);
-  Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected..!");
-  Serial.print("Got IP: ");  
-  Serial.println(WiFi.localIP());
-  client.setServer(mqtt_host, mqtt_port);
 
   // Display Setup
   String LVGL_Arduino = String("LVGL Library Version: ") + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
   Serial.println(LVGL_Arduino);
   lv_init();
-  lv_display_t * disp;
+  lv_display_t *disp;
   disp = lv_tft_espi_create(SCREEN_WIDTH, SCREEN_HEIGHT, draw_buf, sizeof(draw_buf));
   lv_display_set_rotation(disp, LV_DISPLAY_ROTATION_270);
-  // drawImage();
+  wifi_status_label = lv_label_create(lv_scr_act());
+  mqtt_status_label = lv_label_create(lv_scr_act());
+  lv_label_set_text(wifi_status_label, "WiFi: Connecting...");
+  lv_obj_align(wifi_status_label, LV_ALIGN_BOTTOM_MID, 0, -30);
+  lv_label_set_text(mqtt_status_label, "MQTT: Connecting...");
+  lv_obj_align(mqtt_status_label, LV_ALIGN_BOTTOM_MID, 0, -10);
+  drawImage();
+
+  // WiFi and MQTT Setup
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected..!");
+  Serial.print("Got IP: ");  
+  Serial.println(WiFi.localIP());
+  lv_label_set_text(wifi_status_label, "WiFi: Connected");
+  client.setServer(mqtt_host, mqtt_port);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   lv_task_handler(); 
   lv_tick_inc(1); 
 
@@ -65,40 +69,38 @@ void loop() {
     reconnect();
   }
   client.loop();
-
   int sensorValue = readEMG();
-  // Serial.println(sensorValue);
   String sensorValueStr = String(sensorValue);
+  Serial.println(sensorValue);
   client.publish("esp32/data", (const uint8_t*)sensorValueStr.c_str(), sensorValueStr.length());
   delay(1);
 }
 
 int readEMG() {
   return analogRead(EMG_PIN);
-
-  // use random value when sensor is not connected
-  // return random(0, 1024);
 }
 
 void drawImage(void) {
   LV_IMAGE_DECLARE(my_image);
-  lv_obj_t * img1 = lv_image_create(lv_screen_active());
-  lv_image_set_src(img1, &my_image);
-  lv_obj_align(img1, LV_ALIGN_CENTER, 0, 0);
+  lv_obj_t *img1 = lv_img_create(lv_scr_act());
+  lv_img_set_src(img1, &my_image);
+  lv_obj_align(img1, LV_ALIGN_CENTER, 0, -30);
 }
 
 void reconnect() {
-  // Loop until we're reconnected
+  // Loop until MQTT is reconnected
   while (!client.connected()) {
+    drawImage();
     Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
+    lv_label_set_text(mqtt_status_label, "MQTT: Connecting...");
+
     if (client.connect("myosense-esp32")) {
       Serial.println("connected");
+      lv_label_set_text(mqtt_status_label, "MQTT: Connected"); 
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
       delay(5000);
     }
   }
