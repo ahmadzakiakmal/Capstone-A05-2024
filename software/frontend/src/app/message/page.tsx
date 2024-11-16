8b34d3270b218c95ca016094216f3a3454d66ae0
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-11-16 20:42:44 +0700

    feat: add message page for breaks

---
+++ software/frontend/src/app/message/page.tsx
@@ -0,0 +1,7 @@
+export default function Message() {
+  return(
+    <main className="min-h-screen  flex flex-col justify-center items-center">
+      <h1 className="text-center animate-pulse text-4xl">AFK</h1>
+    </main>
+  )
+}
